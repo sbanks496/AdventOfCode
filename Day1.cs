70152c8cf9443af7c2f5ec74e58e4f8fcfae1e0c
sbanks496 <100219535+sbanks496@users.noreply.github.com>
2022-12-01 21:49:30 +0000

    Day 1 Part 2
    Modify the Part 1 method to be a bit more complicated in order to solve
    Part 2.  Should try to tidy this properly at some point.

--- Day1.cs
+++ Day1.cs
@@ -26,4 +26,33 @@ public class Day1
         }
         Console.WriteLine(string.Join(',', maxTotal));
     }
+    public static void Part2(string fileName, int topElves)
+    {
+        string[] lines = ReadFromFile.ReadLines(fileName);
+        int runningTotal = 0;
+        List<Tuple<int,int>> elfSnacks = new List<Tuple<int,int>> { new (0,0)};
+        int step = 1;
+        foreach (string line in lines)
+        {
+            if (line == "")
+            {
+                elfSnacks.Add(new(runningTotal, step));
+                step += 1;
+                runningTotal = 0;
+            }
+            else
+            {
+                runningTotal += int.Parse(line);
+            }
+        }
+        elfSnacks.Sort((x, y) => y.Item1.CompareTo(x.Item1));
+        int topSnacks = 0;
+        for (int i = 1; i <= topElves; i++)
+        {
+            Console.WriteLine(string.Join(',', elfSnacks[i-1]));
+            topSnacks += elfSnacks[i - 1].Item1;
+        }
+        Console.Write(topSnacks);
+    }
+
 }
--- Program.cs
+++ Program.cs
@@ -4,3 +4,4 @@ using AdventOfCode;
 
 Console.WriteLine("Hello, World!");
 Day1.Part1("day1.txt");
+Day1.Part2("day1.txt", 3);
